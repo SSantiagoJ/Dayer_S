39c630b4c670bc9747f0f891fdba61e180fb6058
esanzh23 <84424030+esanzh23@users.noreply.github.com>
2024-06-27 21:37:45 -0500

    Add files via upload

---
+++ sublimado/src/test/java/com/sublimado/app/SublimadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sublimado.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SublimadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
